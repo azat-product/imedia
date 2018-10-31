f08701a11d2593e95918e27104850375fcd6d9cb
Maksym Shevchyk <maksym.shevchyk@clazion.com>
2018-10-31 09:46:51 +0200

    IK-10 [Объявление] Некорректное поведение счетчика рейтинга\звёзд рейтинга

--- themes/imedia_t12d0f3/modules/bbs/tpl/def/item.rating.author.cat.avarage.php
+++ themes/imedia_t12d0f3/modules/bbs/tpl/def/item.rating.author.cat.avarage.php
@@ -1,7 +1,7 @@
 <div class="mrgt10 star-rating-author-cat-avarage star-imedia">
     <? $intValue = round($value);  ?>
     <? for($i = 1; $i <= 5; $i++): ?>
-        <span class="fa fa-star <?= $intValue >= $i ? 'active' : 'active'?>" data-rating="<?=$i?>"></span>
+        <span class="fa fa-star <?= $intValue >= $i ? 'active' : ''?>" data-rating="<?=$i?>"></span>
     <? endfor;?>
     (<span><?=number_format($value, 2)?></span>)
 </div>
